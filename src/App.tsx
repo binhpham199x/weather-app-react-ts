c4e554dc2960bbef21e09e325aae237fde9e4416
Binh Pham <binhpham199x@gmail.com>
2024-05-14 16:43:31 +0200

    feat: add basic get request for weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+// import request from "request";
+
+import { useEffect, useState } from "react";
+
 function App() {
-   const text = import.meta.env.VITE_API_KEY;
+   const API_KEY = import.meta.env.VITE_API_KEY;
+
+   const name = "hanoi";
+
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API_KEY}`;
+
+   const [data, setData] = useState();
+
+   const fetchData = async (): Promise<void> => {
+      try {
+         const response: Response = await fetch(url, {
+            method: "GET",
+         });
+
+         if (!response.ok) {
+            throw new Error("Network response was not ok");
+         }
+
+         setData(await response.json()); // Replace 'any' with the actual type of your response data
+      } catch (error) {
+         console.error("Error:", error);
+      }
+   };
+   console.log(data);
+
+   useEffect(() => {
+      fetchData();
+   }, []);
+
    return (
       <>
-         <h1 className="text-3xl font-bold underline">Hello world! {text} </h1>
+         <h1 className="text-3xl font-bold underline">Hello world! </h1>
+
+         <pre id="json">{JSON.stringify(data, undefined, 2)}</pre>
       </>
    );
 }
