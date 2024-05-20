3605a764fb6e7829a73968ce725c28c92c13568f
binhpham199x <binhpham199x@gmail.com>
2024-05-20 16:15:08 +0200

    feat: adjust function logic for fetchWeatherData

--- src/services/OpenWeatherService.ts
+++ src/services/OpenWeatherService.ts
@@ -5,32 +5,37 @@ const WEATHER = "/weather";
 const FORECAST = "/forecast";
 
 type Units = "standard" | "metric" | "imperial";
-type ForecastType = "today" | "weekly";
 
 export const fetchWeatherData = async (
-   forecastType: ForecastType,
-   lat: number,
-   lon: number,
-   units: Units = "metric",
-   cnt: number = 40
-): Promise<unknown> => {
-   const SEGMENT = forecastType == "today" ? WEATHER : FORECAST;
+	lat: number,
+	lon: number,
+	units: Units = "metric",
+	cnt: number = 40
+) => {
+	const urlWeekly =
+		OPEN_WEATHER_URL +
+		FORECAST +
+		`?lat=${lat}&lon=${lon}&cnt=${cnt}&units=${units}&appid=${OPEN_WEATHER_API_KEY}`;
 
-   const url =
-      OPEN_WEATHER_URL +
-      SEGMENT +
-      `?lat=${lat}&lon=${lon}&cnt=${cnt}&units=${units}&appid=${OPEN_WEATHER_API_KEY}`;
-   try {
-      const response = await fetch(url);
+	const urlToday =
+		OPEN_WEATHER_URL +
+		WEATHER +
+		`?lat=${lat}&lon=${lon}&units=${units}&appid=${OPEN_WEATHER_API_KEY}`;
 
-      if (!response.ok) {
-         throw new Error("Network response was not ok");
-      }
+	try {
+		const [weatherPromise, forecastPromise] = await Promise.all([
+			fetch(urlToday),
+			fetch(urlWeekly),
+		]);
+		const weatherResponse = await weatherPromise.json();
+		const forcastResponse = await forecastPromise.json();
 
-      const res = await response.json();
-      return res;
-      
-   } catch (error) {
-      console.error("Error:", error);
-   }
+		if (!weatherResponse.ok || !forcastResponse.ok) {
+			throw new Error("Network response was not ok");
+		}
+
+		return [weatherResponse, forcastResponse];
+	} catch (error) {
+		console.error("Error:", error);
+	}
 };
